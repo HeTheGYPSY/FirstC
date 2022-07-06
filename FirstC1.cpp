c732854059254fc3ed6006c8ce599412ed1af24f
Vernon Hezron <vernonhezron@gmail.com>
2022-07-06 22:37:01 +0300

    First Commit

--- FirstC1.cpp
+++ FirstC1.cpp
@@ -3,8 +3,7 @@
 //
 #include <iostream>
 using namespace std;
-int main()
-{
+int main() {
     cout << "Size of char : " << sizeof(char) << endl;
     cout << "Size of int : " << sizeof(int) << endl;
     cout << "Size of short int : " << sizeof(short int) << endl;
@@ -12,15 +11,5 @@ int main()
     cout << "Size of float : " << sizeof(float) << endl;
     cout << "Size of double : " << sizeof(double) << endl;
     cout << "Size of wchar_t : " << sizeof(wchar_t) << endl;
-    return#include <iostream>
-    using namespace std;
-    int main()
-    {
-        cout << "Size of char : " << sizeof(char) << endl;
-        cout << "Size of int : " << sizeof(int) << endl;
-        cout << "Size of short int : " << sizeof(short int) << endl;
-        cout << "Size of long int : " << sizeof(long int) << endl;
-        cout << "Size of float : " << sizeof(float) << endl;
-        cout << "Size of double : " << sizeof(double) << endl;
-        cout << "Size of wchar_t : " << sizeof(wchar_t) << endl;
-        return 0; 0;
+    return 0;
+}
