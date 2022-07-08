ec4a2088833a07bfac0f9dad22659ea2daaf6f24
Vernon Hezron <vernonhezron@gmail.com>
2022-07-08 09:44:39 +0300

    Third Commit 3

--- FirstC1.cpp
+++ FirstC1.cpp
@@ -1,16 +1,3 @@
 //
 // Created by User on 7/6/2022.
 //
-
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-int main() {
-    string resp;
-    cout << "What do you want to say? " << "\n";
-    cin >> resp;
-    cout << "Okay then, we'll " << resp;
-    return 0;
-}
