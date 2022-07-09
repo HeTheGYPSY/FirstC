a56300318ec04494065db41bbf8c47b9cf8f0e65
VERNON HEZRON <103187250+HeTheGYPSY@users.noreply.github.com>
2022-07-09 13:39:57 +0300

    Python connections

--- FirstC7.py
+++ FirstC7.py
@@ -1,4 +1,7 @@
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.recv(1000)
+s.bind(socket.gethostbyname(socket.gethostname()))
+s.listen(50)
+s.accept()
+s.connect(socket.gethostbyname(socket.gethostname()))
 print("Connected")
