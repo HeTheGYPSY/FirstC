24f541dc7200f162c71847ae2f4e77cd25d01972
VERNON HEZRON <103187250+HeTheGYPSY@users.noreply.github.com>
2022-07-10 13:17:37 +0300

    Python connections

--- FirstC7.py
+++ FirstC7.py
@@ -1,7 +1,25 @@
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(socket.gethostbyname(socket.gethostname()))
-s.listen(50)
-s.accept()
-s.connect(socket.gethostbyname(socket.gethostname()))
-print("Connected")
+import hashlib
+import os
+print("**************PASSWORD CRACKER ******************")
+pass_found = 0  # To check if the password  found or not
+input_hash = input("Enter the hashed password:")
+pass_doc = input("\nEnter passwords filename including path(root / home/): ")
+if os.access(pass_doc, os.R_OK) is True:
+    try:
+        pass_file = open(pass_doc, 'r')
+
+        for word in pass_file:  # comparing the input_hash with the hashes of the words in password file
+            enc_word = word.encode('utf-8')
+            hash_word = hashlib.md5(enc_word.strip())
+            digest = hash_word.hexdigest()  # digesting that hash into a hexadecimal value
+            if digest == input_hash:
+                print("Password found.\nThe password is:", word)  # comparing hashes
+                pass_found = 1
+                break
+
+        if not pass_found:
+            print("Password is not found in the", pass_doc, "file")
+            print('\n')
+        print("*****************  Thank you  **********************")
+    except PermissionError as error:
+        print(error)
